a765df9a79cd5fe13bf6c4e051d77bf9f7fb1a02
Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>
2024-11-20 14:45:14 +0100

    svd: add helper for populating and outputting device memory
    Adds a utility module with helpers for populating and outputting
    device memory.
    
    The following formats are supported for input/output:
    * A dict mapping address -> value (at a byte level)
    * A dict mapping a device-like structure to register/field values
    
    The following format is currently only supported for output:
    * A bytearray containing device memory
    
    The APIs for generating outputs offer a flexible "selector"
    mechanism that lets the user decide which part of the device to
    output.
    
    Signed-off-by: Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>

--- src/svd/__init__.py
+++ src/svd/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2022 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
+from . import util
 from .bindings import (
     Access,
     ReadAction,
@@ -94,6 +95,7 @@ __all__ = [
     "RegisterType",
     "Struct",
     # other
+    "util",
     "__version__",
 ]
 
